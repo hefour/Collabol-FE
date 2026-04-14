403db79628920ec58795d9c19f705a94606e85fe
hefour <jhec0113@naver.com>
2026-04-14 15:29:14 +0900

    feat: 인증 라우팅 추가 (/login, /register)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/layout/Layout'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 import HomePage from './pages/HomePage'
 import ProjectsPage from './pages/ProjectsPage'
 import ProfilePage from './pages/ProfilePage'
@@ -8,15 +10,22 @@ import SharePage from './pages/SharePage'
 
 export default function App() {
   return (
-      <Routes>
-        <Route path="/profile/:userId/share" element={<SharePage />} />
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/review" element={<ReviewPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
+    <Routes>
+      {/* 인증 */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      {/* 공개 */}
+      <Route path="/profile/:userId/share" element={<SharePage />} />
+
+      {/* 앱 (사이드바 레이아웃) */}
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/review" element={<ReviewPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
   )
 }
