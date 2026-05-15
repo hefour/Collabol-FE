7309bbb8366833e86eb5aee16d8da043ecab57d7
dsap1234 <atktony2013@gmail.com>
2026-05-15 15:43:37 +0900

    Add Evaluate and ProjectDetail pages to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import HomePage from './pages/HomePage'
-import ProjectsPage from './pages/ProjectsPage'
-import ProfilePage from './pages/ProfilePage'
-import ReviewPage from './pages/ReviewPage'
-import SharePage from './pages/SharePage'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ProfilePage from "./pages/ProfilePage";
+import ReviewPage from "./pages/ReviewPage";
+import SharePage from "./pages/SharePage";
+import EvaluatePage from "./pages/EvaluatePage";
+import ProjectDetailPage from "./pages/ProjectDetailPage";
 
 export default function App() {
   return (
     <Routes>
-      {/* 인증 */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-
-      {/* 공개 */}
       <Route path="/profile/:userId/share" element={<SharePage />} />
 
-      {/* 앱 (사이드바 레이아웃) */}
       <Route element={<Layout />}>
         <Route path="/home" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/review" element={<ReviewPage />} />
+        <Route path="/review/:project" element={<ReviewPage />} />
+        <Route path="/evaluate/:project" element={<EvaluatePage />} />
+        <Route path="/projects/:project" element={<ProjectDetailPage />} />
       </Route>
 
-      {/* 기본 경로 → 로그인 */}
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
-  )
+  );
 }
